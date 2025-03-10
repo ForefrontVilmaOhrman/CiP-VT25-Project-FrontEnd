e56c0a3adff10c27eee70bb96547e2337467ef35
Mahmoud Daabas <mahmoud.daabas@forefront.se>
2025-03-10 15:49:38 +0100

    style filtering and add navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EventDetailsComponent } from '../components/event-details/event-details.component';
+import { TestComponentComponent } from '../components/test-component/test-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Define routes used by the application.
+  { path: 'event/:id', component: EventDetailsComponent },
+  { path: '**', component: TestComponentComponent }
+];
