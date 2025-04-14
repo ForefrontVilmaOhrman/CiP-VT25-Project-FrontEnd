16320b801179bfc1b3e76f2d65bc61a6622b3f67
Mahmoud Daabas <mahmoud.daabas@forefront.se>
2025-04-14 15:39:20 +0200

    add possibility to delete event

--- src/components/event-details/event-details.component.ts
+++ src/components/event-details/event-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AppEvent } from '../../models/app-event';
 import { RestService } from '../../services/rest.service';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'app-event-details',
@@ -15,11 +16,20 @@ export class EventDetailsComponent implements OnInit {
 
   appEvent: AppEvent | undefined;
 
-  constructor(private route: ActivatedRoute, private restService: RestService) {}
+  constructor(private route: ActivatedRoute, private restService: RestService, private navigationService: NavigationService) { }
 
   async ngOnInit(): Promise<any> {
     const eventId = this.route.snapshot.paramMap.get('id'); // Use this ID to fetch data about event. Maybe local storage check before getting?
     const allAppEvents: AppEvent[] = await this.restService.getData();
     this.appEvent = allAppEvents.find(event => event.id === eventId)
+  }
+
+  handleDeleteEvent(eventId: string): void {
+    this.restService.deleteEvent(eventId).then(() => {
+      this.navigationService.navigateToHomepage();
+    }
+    ).catch((error) => {
+      console.error("Error deleting event", error);
+    });
   }
 }
